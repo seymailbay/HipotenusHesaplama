22b4c2a45a490953baf5fe1fcd3a92ad40633135
Seymaİlbay <seymailbay07@gmail.com>
2022-06-27 12:16:44 +0300

    Create hipotenüs.cpp

---
+++ Hipotenus/hipotenüs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+main () {
+	double a,b,c;
+	
+	cout << " 1. kenari giriniz";
+	cin >> a;
+	cout << "2.kenari giriniz";
+	cin >> b;
+	c = sqrt ( pow (a,2) + pow (b,2)       );
+	
+	cout << " hipotenüs = " << c ;
+	
+}
